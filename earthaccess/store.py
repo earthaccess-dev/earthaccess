588db9502eecc55d689819fc4fedf004cfff8e08
Matt Fisher <mfisher87@gmail.com>
2026-08-20 15:40:27 -0600

    Fixup incorrect Any annotation

--- earthaccess/store.py
+++ earthaccess/store.py
@@ -238,7 +238,7 @@ def _sibling_tempfile(sibling: Path) -> Generator[Path, None, None]:
 class Store:
     """Store class to access granules on-prem or in the cloud."""
 
-    def __init__(self, auth: Any, pre_authorize: bool = False) -> None:  # noqa: FBT001, FBT002
+    def __init__(self, auth: Auth, pre_authorize: bool = False) -> None:  # noqa: FBT001, FBT002
         """Store is the class to access data.
 
         Parameters:
